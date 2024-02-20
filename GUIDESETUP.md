274a77b0bd0d298f65f73744bf44cf9701047e34
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-20 04:08:49 +0000

    GuideSetup

---
+++ GUIDESETUP.md
@@ -0,0 +1,2 @@
+# Setup
+[Github Link](https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/adding-a-dev-container-configuration/setting-up-your-java-project-for-codespaces)
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,10 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World_testing!");
+       //System.out.println("Hello Remote World_testing!");
+       //System.out.println(123);
+
+       System.out.println();
+       System.out.println("hello");
     }
 }
